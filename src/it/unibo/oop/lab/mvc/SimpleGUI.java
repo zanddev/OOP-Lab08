509eeac4aaf79b484a1066feeaa467267f74e636
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-11-21 16:23:36 +0100

    done GUI for mvc in class SimpleGUI

--- src/it/unibo/oop/lab/mvc/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvc/SimpleGUI.java
@@ -1,9 +1,17 @@
 package it.unibo.oop.lab.mvc;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -11,23 +19,21 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final String TITLE = "I/O GUI application";
+    private final JFrame frame = new JFrame(TITLE);
+    private final Controller controller = new ControllerImpl();
 
     /*
      * Once the Controller is done, implement this class in such a way that:
      * 
-     * 1) I has a main method that starts the graphical application
-     * 
-     * 2) In its constructor, sets up the whole view
-     * 
      * 3) The graphical interface consists of a JTextField in the upper part of the frame, 
-     * a JTextArea in the center and two buttons below it: "Print", and "Show history". 
+     * a JTextArea in the centre and two buttons below it: "Print", and "Show history". 
      * SUGGESTION: Use a JPanel with BorderLayout
      * 
      * 4) By default, if the graphical interface is closed the program must exit
      * (call setDefaultCloseOperation)
      * 
-     * 5) The behavior of the program is that, if "Print" is pressed, the
+     * 5) The behaviour of the program is that, if "Print" is pressed, the
      * controller is asked to show the string contained in the text field on standard output. 
      * If "show history" is pressed instead, the GUI must show all the prints that
      * have been done to this moment in the text area.
@@ -35,10 +41,57 @@ public final class SimpleGUI {
      */
 
     /**
+     * 2) In its constructor, sets up the whole view
      * builds a new {@link SimpleGUI}.
      */
     public SimpleGUI() {
 
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        final JTextField field = new JTextField();
+        panel.add(field, BorderLayout.NORTH);
+
+        final JTextArea area = new JTextArea();
+        area.setEditable(false);
+        panel.add(area, BorderLayout.CENTER);
+
+        final JPanel bar = new JPanel();
+        bar.setLayout(new BorderLayout());
+        panel.add(bar, BorderLayout.SOUTH);
+
+        final JButton print = new JButton("Print");
+        final JButton history = new JButton("Show history");
+        bar.add(print, BorderLayout.WEST);
+        bar.add(history);
+
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        print.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                final String text = field.getText();
+                controller.setNextLine(text);
+                controller.printCurrentLine();
+                area.setText(text);
+            }
+        });
+        history.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                final List<String> lines = controller.getPreviousLines();
+                //String prevs = "";
+                final StringBuilder prev = new StringBuilder();
+                for (final String line : lines) {
+                   //prevs += line + "\n";
+                   prev.append(line);
+                   prev.append('\n');
+                }
+                //System.out.println(prevs);
+                area.setText(prev.toString());
+            }
+        });
         /*
          * Make the frame half the resolution of the screen. This very method is
          * enough for a single screen setup. In case of multiple monitors, the
@@ -60,6 +113,17 @@ public final class SimpleGUI {
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+        /*
+         * OK, ready to pull the frame onscreen
+         */
+        frame.setVisible(true);
     }
 
+    /**
+     * 1) Main method that starts the graphical application.
+     * @param args ignored
+     */
+    public static void main(final String... args) {
+       new SimpleGUI();
+    }
 }
